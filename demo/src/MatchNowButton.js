a3d106ad468b490bc2338c78f98bdce17ad4893f
dishidhak <dishidhak@gmail.com>
2025-03-29 17:25:04 -0400

    new file:   demo/.gitignore new file:   demo/.vscode/launch.json new file:   demo/README.md 	new file:   demo/package-lock.json 	new file:   demo/package.json 	new file:   demo/public/favicon.ico 	new file:   demo/public/index.html 	new file:   demo/public/logo192.png 	new file:   demo/public/logo512.png 	new file:   demo/public/manifest.json 	new file:   demo/public/robots.txt 	new file:   demo/src/App.css 	new file:   demo/src/App.js new file:   demo/src/App.test.js new file:   demo/src/CavaLinkLogo.png new file:   demo/src/MatchNowButton.css new file:   demo/src/MatchNowButton.js new file:   demo/src/index.css new file:   demo/src/index.js 	new file:   demo/src/logo.svg 	new file:   demo/src/reportWebVitals.js 	new file:   demo/src/setupTests.js

---
+++ demo/src/MatchNowButton.js
@@ -0,0 +1,13 @@
+// src/MatchNowButton.js
+import React from 'react';
+import './MatchNowButton.css';  // Import the updated CSS file
+
+const MatchNowButton = () => {
+  return (
+    <button className="match-now-btn">
+      Match Now!
+    </button>
+  );
+}
+
+export default MatchNowButton;

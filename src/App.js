991618feb3fb01c876f4aa7ee900f69ce6066fb9
IshneetB <ishneetbhatia@gmail.com>
2025-03-29 14:35:34 -0400

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="app-content">
-        <h1 className="cavalink-title">CavaLink</h1>
-        <div className="weather-alert">
-          <p>Fire weather stat...</p>
-          <p className="alert-status">In effect</p>
-        </div>
-        <div className="search-container">
-          <input type="text" placeholder="Search" />
-        </div>
-      </div>
+      <h1 className="cavalink-title">CavaLink</h1>
     </div>
   );
 }
 
-// Make sure this line exists exactly like this:
 export default App;

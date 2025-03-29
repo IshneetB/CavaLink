c714cd15bce97382052688ce7f7ad81c2c330495
IshneetB <ishneetbhatia@gmail.com>
2025-03-29 14:44:34 -0400

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="#about">ABOUT</a></li>
+          <li><a href="#team">OUR TEAM</a></li>
+          <li><a href="#spotlight">SPOTLIGHT</a></li>
+          <li><a href="#explore">EXPLORE</a></li>
+          <li><a href="#contact">CONTACT</a></li>
+        </ul>
+      </nav>
       <h1 className="cavalink-title">CavaLink</h1>
     </div>
   );

9ed2b94d4adfc2a620b209c3541541d0957bf08a
IshneetB <ishneetbhatia@gmail.com>
2025-03-29 14:00:50 -0400

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Your Content Here</h1>
+        <h1>CavaLink</h1>
       </header>
     </div>
   );

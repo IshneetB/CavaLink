587d60ba2a66386b25dd750abcde7b4f96633daf
IshneetB <ishneetbhatia@gmail.com>
2025-03-29 14:27:31 -0400

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="font-dmserif text-xl">Cavalink UI</p>
-      </header>
+      <div className="app-content">
+        <h1 className="cavalink-title">CavaLink</h1>
+        <div className="weather-alert">
+          <p>Fire weather stat...</p>
+          <p className="alert-status">In effect</p>
+        </div>
+        <div className="search-container">
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
     </div>
   );
 }
 
+// Make sure this line exists exactly like this:
 export default App;
